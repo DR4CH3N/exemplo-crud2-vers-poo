a6d50bb33ed4c60f12f9402d81a64d27b89a9d42
DR4CH3N <thehellc4t@gmail.com>
2022-07-18 08:39:34 -0300

    colocando bootstrap com composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,38 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '3e7edc0dcf57ac0f29109b5a109fbd5774e3c10d',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '3e7edc0dcf57ac0f29109b5a109fbd5774e3c10d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'twbs/bootstrap' => array(
+            'pretty_version' => 'v5.2.0-beta1',
+            'version' => '5.2.0.0-beta1',
+            'reference' => 'fca04c07131a81e625a516cf7b98a8c7df0df1c1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../twbs/bootstrap',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'twitter/bootstrap' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => 'v5.2.0-beta1',
+            ),
+        ),
+    ),
+);
